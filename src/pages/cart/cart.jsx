ff3fafc944cf24f9b1f05d9993beaf76e1b99208
DanielStyles3 <danielstyles318@gmail.com>
2023-09-20 13:19:50 +0100

    updated my code

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -5,6 +5,7 @@ import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
 
 import "./cart.css";
+
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
@@ -12,34 +13,34 @@ export const Cart = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="cart">
-      <div>
+    <div className="cart-container">
+      <div className="cart-content">
         <h1>Your Cart Items</h1>
       </div>
-      <div className="cart">
+      <div className="cart-items">
         {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
+            return <CartItem data={product} key={product.id} />;
           }
+          return null; // Don't forget to add a key prop to the mapped elements.
         })}
       </div>
 
       {totalAmount > 0 ? (
         <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
           <button
             onClick={() => {
               checkout();
               navigate("/checkout");
             }}
           >
-            {" "}
-            Checkout{" "}
+            Checkout
           </button>
         </div>
       ) : (
-        <h1> Your Shopping Cart is Empty</h1>
+        <h1>Your Shopping Cart is Empty</h1>
       )}
     </div>
   );
